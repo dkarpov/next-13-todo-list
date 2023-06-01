99b1b63439e607e896a536c19efc7785ffb23cb1
dkarpov <dimon.karpov1@gmail.com>
2023-06-01 13:52:50 +0200

    add delete functionality and refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { TodoItem } from "@/components/TodoItem"
 import { prisma } from "@/db"
 import Link from "next/link"
+import { useRouter } from "next/router";
+
+export const revalidate = 500; // revalidate every hour
 
 function getTodos() {
   return prisma.todo.findMany()
@@ -9,7 +12,13 @@ function getTodos() {
 async function toggleTodo(id: string, complete: boolean) {
   "use server"
 
-  await prisma.todo.update({ where: { id }, data: { complete } })
+  return await prisma.todo.update({ where: { id }, data: { complete } })
+}
+
+async function removeTodo(id: string) {
+  "use server"
+
+  return await prisma.todo.delete({ where: { id } })
 }
 
 export default async function Home() {
@@ -28,7 +37,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map(todo => (
-          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} removeTodo={removeTodo} />
         ))}
       </ul>
     </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,38 @@
 "use client"
 
+import { useRouter } from "next/navigation"
+import { useState, useTransition } from "react"
+
 type TodoItemProps = {
   id: string
   title: string
   complete: boolean
   toggleTodo: (id: string, complete: boolean) => void
+  removeTodo: (id: string) => void
 }
 
-export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
+export function TodoItem({ id, title, complete, toggleTodo, removeTodo }: TodoItemProps) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+
+  // Create inline loading UI
+  const isMutating = isFetching || isPending;
+
+  async function handleDelete() {
+    setIsFetching(true);
+    // Mutate external data source
+    await removeTodo(id);
+    setIsFetching(false);
+
+    startTransition(() => {
+      // Refresh the current route and fetch new data from the server without
+      // losing client-side browser or React state.
+      router.refresh();
+    });
+  }
   return (
-    <li className="flex gap-1 items-center">
+    <li className="flex gap-1 items-center" style={{ opacity: !isMutating ? 1 : 0.7 }}>
       <input
         id={id}
         type="checkbox"
@@ -23,6 +46,7 @@ export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
       >
         {title}
       </label>
+      <button className="border-2 rounded p-0.5 text-xs" onClick={(evt) => handleDelete()}>Remove Todo</button>
     </li>
   )
 }
